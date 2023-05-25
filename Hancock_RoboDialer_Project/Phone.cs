91f29236d68e7d740edbc92f6e0dcdafd77abab7
Brittany Hancock <brittany.hancock@student.centralia.edu>
2023-05-25 07:35:56 -0700

    Updated Phone instance data to explicit private access

--- Hancock_RoboDialer_Project/Phone.cs
+++ Hancock_RoboDialer_Project/Phone.cs
@@ -2,10 +2,10 @@
 {
     internal abstract class Phone
     {
-        string[] _phoneNumber = new string[10];
-        string[] _companyName = new string[10];
-        string[] _phoneType = new string[10];
-        int contactIndex = -1;
+        private string[] _phoneNumber = new string[10];
+        private string[] _companyName = new string[10];
+        private string[] _phoneType = new string[10];
+        private int contactIndex = -1;
 
         public Phone(string PhoneNumber, string CompanyName, string PhoneType)
         {
